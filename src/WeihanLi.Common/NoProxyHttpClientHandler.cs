4d30419249bd5a1802dcd7dd3025c3afde9cc9b7
liweihan <weihanli@outlook.com>
2019-05-19 15:53:45 +0800

    add NoProxyHttpClientHandler.cs

---
+++ src/WeihanLi.Common/NoProxyHttpClientHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Http;
+
+namespace WeihanLi.Common
+{
+    public class NoProxyHttpClientHandler : HttpClientHandler
+    {
+        public NoProxyHttpClientHandler()
+        {
+            Proxy = null;
+            UseProxy = false;
+        }
+    }
+}
