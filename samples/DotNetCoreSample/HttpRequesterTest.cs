874478ae07d4e1ff678fb0c3596cec7cf3317ad0
weihanli <weihanli@outlook.com>
2019-05-31 21:24:15 +0800

    update HttpRequesterExtensions namespace

--- samples/DotNetCoreSample/HttpRequesterTest.cs
+++ samples/DotNetCoreSample/HttpRequesterTest.cs
@@ -2,6 +2,7 @@
 using System.Net.Http;
 using WeihanLi.Common.Helpers;
 using WeihanLi.Extensions;
+using WeihanLi.Common.Extensions;
 
 namespace DotNetCoreSample
 {
@@ -29,23 +30,23 @@ namespace DotNetCoreSample
                 .Execute();
             System.Console.WriteLine(result);
 
-            //var loginResult = new WebRequestHttpRequester("https://accounting.weihanli.xyz/Account/LogOn", HttpMethod.Post)
-            //    .WithHeaders(new Dictionary<string, string>()
-            //    {
-            //        // { "X-Requested-With", "XMLHttpRequest" },
-            //        {"Header1", "Header1" }
-            //    })
-            //    .AjaxRequest()
-            //    .WithReferer("https://accounting.weihanli.xyz/Account/Login?ReturnUrl=%2F")
-            //    .WithUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36")
-            //    .WithParameters(new Dictionary<string, string>()
-            //    {
-            //        {"Username","liweihan" },
-            //        {"Password", "112233" },
-            //        {"RememberMe","false" }
-            //    })
-            //    .ExecuteForJson<WeihanLi.Common.Models.JsonResultModel<bool>>();
-            //System.Console.WriteLine(loginResult.ToJson());
+            var loginResult = new WebRequestHttpRequester("https://accounting.weihanli.xyz/Account/LogOn", HttpMethod.Post)
+               .WithHeaders(new Dictionary<string, string>()
+               {
+                   // { "X-Requested-With", "XMLHttpRequest" },
+                   {"Header1", "Header1" }
+               })
+               .AjaxRequest()
+               .WithReferer("https://accounting.weihanli.xyz/Account/Login?ReturnUrl=%2F")
+               .WithUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36")
+               .WithFormParams(new Dictionary<string, string>()
+               {
+                   {"Username","liweihan" },
+                   {"Password", "112233" },
+                   {"RememberMe","false" }
+               })
+               .ExecuteForJson<WeihanLi.Common.Models.JsonResultModel<bool>>();
+            System.Console.WriteLine(loginResult.ToJson());
 
             var uploadFileResponse = new WebRequestHttpRequester("https://graph.baidu.com/upload", HttpMethod.Post)
                 .WithFile($@"{System.Environment.GetEnvironmentVariable("USERPROFILE")}\Pictures\4e6ab53e383863ed4d15252039f70423.jpg", "image", new Dictionary<string, string>()
--- samples/DotNetFxSample/HttpHelperSample.cs
+++ samples/DotNetFxSample/HttpHelperSample.cs
@@ -5,6 +5,7 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using WeihanLi.Common.Extensions;
 using WeihanLi.Common.Helpers;
 using WeihanLi.Extensions;
 
