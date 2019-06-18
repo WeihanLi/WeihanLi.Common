72684e32c1dd06cf7c01265d254d5e1d63f3808f
WeihanLi <weihanli@outlook.com>
2019-06-18 14:07:02 +0800

    add DateTimeFormatConverter.cs

---
+++ src/WeihanLi.Common/Json/DateTimeFormatConverter.cs
@@ -0,0 +1,12 @@
+using Newtonsoft.Json.Converters;
+
+namespace WeihanLi.Common.Json
+{
+  public class DateTimeFormatConverter : IsoDateTimeConverter
+  {
+    public DateTimeFormatConverter(string format)
+    {
+      DateTimeFormat = format;
+    }
+  }
+}
