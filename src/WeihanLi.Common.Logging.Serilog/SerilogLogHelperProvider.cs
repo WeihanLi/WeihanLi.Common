c5060b2ba5a64f53faef52a489bf17e3c26b24fa
WeihanLi <weihanli@outlook.com>
2019-07-13 09:35:18 +0800

    add SerilogLogHelperProvider.cs(Not Implemented,wait for a new version Serilog remove licenseheader

---
+++ src/WeihanLi.Common.Logging.Serilog/SerilogLogHelperProvider.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace WeihanLi.Common.Logging.Serilog
+{
+    public class SerilogLogHelperProvider : ILogHelperProvider
+    {
+        private readonly ConcurrentDictionary<int, ILogHelperLogger> _loggers = new ConcurrentDictionary<int, ILogHelperLogger>();
+
+        public ILogHelperLogger CreateLogger(string categoryName)
+        {
+            return _loggers.GetOrAdd(1, t => new SerilogLogHelperLogger());
+        }
+    }
+
+    internal class SerilogLogHelperLogger : ILogHelperLogger
+    {
+        public void Log(LogHelperLevel loggerLevel, string message, Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsEnabled(LogHelperLevel loggerLevel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
