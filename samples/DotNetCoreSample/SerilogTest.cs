50dda93a8fded66ea54d79ed67ea1b8d1b733d7c
WeihanLi <weihanli@outlook.com>
2019-07-24 19:42:14 +0800

    update WeihanLi.Common.Logging.Serilog

---
+++ samples/DotNetCoreSample/SerilogTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Logging;
+using Serilog;
+using WeihanLi.Common.Logging.Serilog;
+
+namespace DotNetCoreSample
+{
+    public static class SerilogTest
+    {
+        public static void MainTest()
+        {
+            ILoggerFactory loggerFactory = new LoggerFactory();
+            SerilogHelper.LogInit(config => config.WriteTo.Console());
+            loggerFactory.AddSerilog();
+
+            var logger = loggerFactory.CreateLogger(typeof(SerilogTest));
+            logger.LogTrace("1111111");
+            logger.LogDebug("1111111");
+            logger.LogInformation("121212331");
+            logger.LogWarning("121212331");
+            logger.LogError("121212331");
+            logger.LogCritical("1213131");
+        }
+    }
+}
---
+++ src/WeihanLi.Common.Logging.Serilog/SerilogLoggerProvider.cs
@@ -0,0 +1,106 @@
+﻿// Copyright(c) .NET Foundation.All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using Microsoft.Extensions.Logging;
+using Serilog.Context;
+using Serilog.Core;
+using Serilog.Events;
+using FrameworkLogger = Microsoft.Extensions.Logging.ILogger;
+
+namespace Serilog.Extensions.Logging
+{
+    /// <summary>
+    /// An <see cref="ILoggerProvider"/> that pipes events through Serilog.
+    /// </summary>
+    [ProviderAlias("Serilog")]
+    public class SerilogLoggerProvider : ILoggerProvider, ILogEventEnricher
+    {
+        internal const string OriginalFormatPropertyName = "{OriginalFormat}";
+        internal const string ScopePropertyName = "Scope";
+
+        // May be null; if it is, Log.Logger will be lazily used
+        private readonly ILogger _logger;
+
+        private readonly Action _dispose;
+
+        /// <summary>
+        /// Construct a <see cref="SerilogLoggerProvider"/>.
+        /// </summary>
+        /// <param name="logger">A Serilog logger to pipe events through; if null, the static <see cref="Log"/> class will be used.</param>
+        /// <param name="dispose">If true, the provided logger or static log class will be disposed/closed when the provider is disposed.</param>
+        public SerilogLoggerProvider(ILogger logger = null, bool dispose = false)
+        {
+            if (logger != null)
+                _logger = logger.ForContext(new[] { this });
+
+            if (dispose)
+            {
+                if (logger != null)
+                    _dispose = () => (logger as IDisposable)?.Dispose();
+                else
+                    _dispose = Log.CloseAndFlush;
+            }
+        }
+
+        public FrameworkLogger CreateLogger(string name)
+        {
+            return new SerilogLogger(this, _logger, name);
+        }
+
+        public IDisposable BeginScope<T>(T state)
+        {
+            if (CurrentScope != null)
+                return new SerilogLoggerScope(this, state);
+
+            // The outermost scope pushes and pops the Serilog `LogContext` - once
+            // this enricher is on the stack, the `CurrentScope` property takes care
+            // of the rest of the `BeginScope()` stack.
+            var popSerilogContext = LogContext.PushProperties(this);
+            return new SerilogLoggerScope(this, state, popSerilogContext);
+        }
+
+        public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+        {
+            List<LogEventPropertyValue> scopeItems = null;
+            for (var scope = CurrentScope; scope != null; scope = scope.Parent)
+            {
+                LogEventPropertyValue scopeItem;
+                scope.EnrichAndCreateScopeItem(logEvent, propertyFactory, out scopeItem);
+
+                if (scopeItem != null)
+                {
+                    scopeItems = scopeItems ?? new List<LogEventPropertyValue>();
+                    scopeItems.Add(scopeItem);
+                }
+            }
+
+            if (scopeItems != null)
+            {
+                scopeItems.Reverse();
+                logEvent.AddPropertyIfAbsent(new LogEventProperty(ScopePropertyName, new SequenceValue(scopeItems)));
+            }
+        }
+
+        private readonly AsyncLocal<SerilogLoggerScope> _value = new AsyncLocal<SerilogLoggerScope>();
+
+        internal SerilogLoggerScope CurrentScope
+        {
+            get
+            {
+                return _value.Value;
+            }
+            set
+            {
+                _value.Value = value;
+            }
+        }
+
+        public void Dispose()
+        {
+            _dispose?.Invoke();
+        }
+    }
+}
