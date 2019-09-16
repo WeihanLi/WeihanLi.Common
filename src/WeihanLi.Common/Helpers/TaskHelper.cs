0c76d511e7030266d88b6daf3be92ae234d36dd4
WeihanLi <weihanli@outlook.com>
2019-09-16 10:08:49 +0800

    add CompletedTask = Task.FromResult(true) for NET45

--- src/WeihanLi.Common/Helpers/TaskHelper.cs
+++ src/WeihanLi.Common/Helpers/TaskHelper.cs
@@ -5,5 +5,11 @@ namespace WeihanLi.Common.Helpers
     public static class TaskHelper
     {
         public static Task<T> FromDefault<T>() => Task.FromResult(default(T));
+
+#if NET45
+
+        public static readonly Task CompletedTask = Task.FromResult(true);
+
+#endif
     }
 }
