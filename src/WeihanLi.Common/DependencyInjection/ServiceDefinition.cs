51e49443dd1150e8c498df1777edc3327847081c
WeihanLi <weihanli@outlook.com>
2019-10-23 18:44:50 +0800

    add DependencyInjection some

---
+++ src/WeihanLi.Common/DependencyInjection/ServiceDefinition.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace WeihanLi.Common.DependencyInjection
+{
+    public class ServiceDefinition
+    {
+        public ServiceLifetime Lifetime { get; }
+
+        public Type ImplementType { get; }
+
+        public Type ServiceType { get; }
+
+        public object ImplementationInstance { get; }
+
+        public Func<IServiceProvider, object> ImplementationFactory { get; }
+    }
+}
