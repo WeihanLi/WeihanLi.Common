2b851436a550c643c511c9408646780562d967b9
WeihanLi <weihanli@outlook.com>
2019-11-22 17:29:44 +0800

    add InterlockedHelper.cs

---
+++ src/WeihanLi.Common/Helpers/InterlockedHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading;
+
+namespace WeihanLi.Common.Helpers
+{
+    public static class InterlockedHelper
+    {
+        public static int Read(ref int value)
+        {
+            return Interlocked.CompareExchange(ref value, 0, 0);
+        }
+
+        public static T Read<T>(ref T value) where T : class
+        {
+            return Interlocked.CompareExchange(ref value, null, null);
+        }
+    }
+}
