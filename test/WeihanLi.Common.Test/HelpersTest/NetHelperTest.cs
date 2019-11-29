dc8d989ee9d552eee671ddbc30859195962afeaa
WeihanLi <weihanli@outlook.com>
2019-11-29 12:47:20 +0800

    add test for IPNetwork

--- test/WeihanLi.Common.Test/HelpersTest/NetHelperTest.cs
+++ test/WeihanLi.Common.Test/HelpersTest/NetHelperTest.cs
@@ -1,4 +1,5 @@
-﻿using WeihanLi.Common.Helpers;
+﻿using System.Net;
+using WeihanLi.Common.Helpers;
 using Xunit;
 
 namespace WeihanLi.Common.Test.HelpersTest
@@ -13,7 +14,16 @@ namespace WeihanLi.Common.Test.HelpersTest
         [InlineData("23.100.91.85", false)]
         public void PrivateIPTest(string ip, bool isPrivate)
         {
-            Assert.Equal(NetHelper.IsPrivateIP(ip), isPrivate);
+            Assert.Equal(isPrivate, NetHelper.IsPrivateIP(ip));
+        }
+
+        [Theory]
+        [InlineData("192.168.0.0/16", "192.168.0.185", true)]
+        [InlineData("192.168.0.0/16", "172.16.0.125", false)]
+        [InlineData("23.100.91.85", "23.100.91.85", true)]
+        public void IPNetworkTest(string cidr, string ip, bool contains)
+        {
+            Assert.Equal(contains, new IPNetwork(cidr).Contains(IPAddress.Parse(ip)));
         }
     }
 }
