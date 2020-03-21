b3889b32fde6bd2d43fbef3ae8a4cf1d8af730fb
weihanli <weihanli@outlook.com>
2020-03-21 21:42:45 +0800

    add UnitOfWork.cs

---
+++ src/WeihanLi.Common/Data/UnitOfWork.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Data.Common;
+using System.Threading;
+using System.Threading.Tasks;
+using WeihanLi.Common.Helpers;
+using WeihanLi.Extensions;
+
+namespace WeihanLi.Common.Data
+{
+    public class UnitOfWork : IUnitOfWork, IDisposable
+    {
+        private readonly DbTransaction _dbTransaction;
+
+        public UnitOfWork(DbConnection dbConnection)
+        {
+            if (null == dbConnection)
+            {
+                throw new ArgumentNullException(nameof(dbConnection));
+            }
+            dbConnection.EnsureOpen();
+            _dbTransaction = dbConnection.BeginTransaction();
+        }
+
+        public UnitOfWork(DbTransaction dbTransaction)
+        {
+            _dbTransaction = dbTransaction ?? throw new ArgumentNullException(nameof(dbTransaction));
+        }
+
+        public void Commit() => _dbTransaction.Commit();
+
+        public Task CommitAsync(CancellationToken cancellationToken = default)
+        {
+            _dbTransaction.Commit();
+            return TaskHelper.CompletedTask;
+        }
+
+        public void Rollback() => _dbTransaction.Rollback();
+
+        public Task RollbackAsync(CancellationToken cancellationToken = default)
+        {
+            _dbTransaction.Rollback();
+            return TaskHelper.CompletedTask;
+        }
+
+        public void Dispose() => _dbTransaction?.Dispose();
+    }
+}
