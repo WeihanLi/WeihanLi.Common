1acd50219d96f183faaecf962a49eb4cd6f9354a
weihanli <weihanli@outlook.com>
2020-03-26 20:52:59 +0800

    remove Obsolete

--- test/WeihanLi.Common.Test/ModelsTest/PagedListDataTest.cs
+++ test/WeihanLi.Common.Test/ModelsTest/PagedListDataTest.cs
@@ -7,11 +7,26 @@ namespace WeihanLi.Common.Test.ModelsTest
     public class PagedListModelTest
     {
         [Fact]
-        public void JsonTest()
+        public void PageDataToJsonTest()
         {
             var model = new PagedListData<int>()
             {
                 PageNum = 1,
+                PageSize = 3,
+                TotalCount = 10,
+                Data = new[] { 1, 2, 3 }
+            };
+            var json = model.ToJson();
+            var dModel = json.JsonToObject<PagedListData<int>>();
+            Assert.Equal(model.PageSize, dModel.PageSize);
+        }
+
+        [Fact]
+        public void PagedModelToJsonTest()
+        {
+            var model = new PagedListModel<int>()
+            {
+                PageNumber = 1,
                 PageSize = 3,
                 TotalCount = 10,
                 Data = new[] { 1, 2, 3 }
