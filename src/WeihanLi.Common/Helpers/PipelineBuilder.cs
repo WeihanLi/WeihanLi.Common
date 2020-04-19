6fffae0b2c37581b892036d8b25eaaabba5b47c2
weihanli <weihanli@outlook.com>
2020-04-19 16:19:55 +0800

    adjust PipelineBuilder structure

---
+++ src/WeihanLi.Common/Helpers/PipelineBuilder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace WeihanLi.Common.Helpers
+{
+    public static class PipelineBuilder
+    {
+        public static IPipelineBuilder<TContext> Create<TContext>(Action<TContext> completeAction)
+        {
+            return new PipelineBuilder<TContext>(completeAction);
+        }
+
+        public static IAsyncPipelineBuilder<TContext> CreateAsync<TContext>(Func<TContext, Task> completeFunc)
+        {
+            return new AsyncPipelineBuilder<TContext>(completeFunc);
+        }
+    }
+}
