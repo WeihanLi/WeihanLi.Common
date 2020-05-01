a65af63e1f0236a47e532f6bcd4203a4a89c28aa
weihanli <weihanli@outlook.com>
2020-05-01 16:58:18 +0800

    add TimeoutAfter extensions

--- src/WeihanLi.Common/Extensions/TaskExtension.cs
+++ src/WeihanLi.Common/Extensions/TaskExtension.cs
@@ -1,4 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Threading;
 using System.Threading.Tasks;
 
 // ReSharper disable once CheckNamespace
@@ -13,5 +17,57 @@ namespace WeihanLi.Extensions
         public static Task WhenAll(this IEnumerable<Task> tasks) => Task.WhenAll(tasks);
 
         public static Task<TResult[]> WhenAll<TResult>(this IEnumerable<Task<TResult>> tasks) => Task.WhenAll(tasks);
+
+        public static async Task<T> TimeoutAfter<T>(this Task<T> task, TimeSpan timeout,
+    [CallerFilePath] string filePath = null,
+    [CallerLineNumber] int lineNumber = default)
+        {
+            // Don't create a timer if the task is already completed
+            // or the debugger is attached
+            if (task.IsCompleted || Debugger.IsAttached)
+            {
+                return await task;
+            }
+
+            var cts = new CancellationTokenSource();
+            if (task == await Task.WhenAny(task, Task.Delay(timeout, cts.Token)))
+            {
+                cts.Cancel();
+                return await task;
+            }
+            else
+            {
+                throw new TimeoutException(CreateMessage(timeout, filePath, lineNumber));
+            }
+        }
+
+        public static async Task TimeoutAfter(this Task task, TimeSpan timeout,
+            [CallerFilePath] string filePath = null,
+            [CallerLineNumber] int lineNumber = default)
+        {
+            // Don't create a timer if the task is already completed
+            // or the debugger is attached
+            if (task.IsCompleted || Debugger.IsAttached)
+            {
+                await task;
+                return;
+            }
+
+            var cts = new CancellationTokenSource();
+            if (task == await Task.WhenAny(task, Task.Delay(timeout, cts.Token)))
+            {
+                cts.Cancel();
+                await task;
+            }
+            else
+            {
+                throw new TimeoutException(CreateMessage(timeout, filePath, lineNumber));
+            }
+        }
+
+        private static string CreateMessage(TimeSpan timeout, string filePath, int lineNumber)
+            => string.IsNullOrEmpty(filePath)
+            ? $"The operation timed out after reaching the limit of {timeout.TotalMilliseconds}ms."
+            : $"The operation at {filePath}:{lineNumber} timed out after reaching the limit of {timeout.TotalMilliseconds}ms.";
     }
 }
