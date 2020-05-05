2083da3a0ae6a182cc2bf045203e8e60ccb8e51b
weihanli <weihanli@outlook.com>
2020-05-05 23:44:49 +0800

    add BaseEntity

---
+++ src/WeihanLi.Common/Models/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace WeihanLi.Common.Models
+{
+    public class BaseEntity<TKey>
+    {
+        public TKey Id { get; set; }
+    }
+
+    public class BaseEntity : BaseEntity<int>
+    {
+    }
+}
