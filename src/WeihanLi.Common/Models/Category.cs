042104839349a3f6fc096201d2a0647c3f60fa60
weihanli <weihanli@outlook.com>
2020-05-14 17:31:29 +0800

    add Category.cs

---
+++ src/WeihanLi.Common/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿namespace WeihanLi.Common.Models
+{
+    public class Category<TKey>
+    {
+        public TKey Id { get; set; }
+
+        public string Name { get; set; }
+
+        public TKey ParentId { get; set; }
+    }
+
+    public class Category : Category<int>
+    {
+    }
+
+    public class CategoryWithDesc : Category<int>
+    {
+        public string Description { get; set; }
+    }
+}
