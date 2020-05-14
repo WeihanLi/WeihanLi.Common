204e9b0d912a8da153d1d12be7fb86385023ca54
weihanli <weihanli@outlook.com>
2020-05-15 01:28:24 +0800

    BaseEntityWithDeleted

--- src/WeihanLi.Common/Models/BaseEntity.cs
+++ src/WeihanLi.Common/Models/BaseEntity.cs
@@ -5,7 +5,18 @@
         public TKey Id { get; set; }
     }
 
+    public class BaseEntityWithDeleted<TKey>
+    {
+        public TKey Id { get; set; }
+
+        public bool IsDeleted { get; set; }
+    }
+
     public class BaseEntity : BaseEntity<int>
+    {
+    }
+
+    public class BaseEntityWithDeleted : BaseEntityWithDeleted<int>
     {
     }
 }
