5cb987666a5dfbe1981ebd4d507983df173b9a4e
weihanli <weihanli@outlook.com>
2020-05-15 13:37:30 +0800

    add Event/IEventSubscriber.cs

--- src/WeihanLi.Common/Event/EventSubscriptionManager.cs
+++ src/WeihanLi.Common/Event/EventSubscriptionManager.cs
@@ -13,32 +13,8 @@ using WeihanLi.Common.Helpers;
 
 namespace WeihanLi.Common.Event
 {
-    public interface IEventSubscriptionManager
+    public interface IEventSubscriptionManager : IEventSubscriber
     {
-        /// <summary>
-        /// add event handler for event
-        /// </summary>
-        /// <returns>whether the operation success</returns>
-        bool Subscribe(Type eventType, Type eventHandlerType);
-
-        /// <summary>
-        /// add event handler for event
-        /// </summary>
-        /// <returns>whether the operation success</returns>
-        Task<bool> SubscribeAsync(Type eventType, Type eventHandlerType);
-
-        /// <summary>
-        /// remove event handler for event
-        /// </summary>
-        /// <returns>whether the operation success</returns>
-        bool UnSubscribe(Type eventType, Type eventHandlerType);
-
-        /// <summary>
-        /// remove event handler for event
-        /// </summary>
-        /// <returns>whether the operation success</returns>
-        Task<bool> UnSubscribeAsync(Type eventType, Type eventHandlerType);
-
         /// <summary>
         /// Get EventHandlers for event
         /// </summary>
@@ -122,58 +98,6 @@ namespace WeihanLi.Common.Event
             where TEvent : class, IEventBase
         {
             return subscriptionManager.GetEventHandlerTypes(typeof(TEvent));
-        }
-
-        /// <summary>
-        /// add event handler for event
-        /// </summary>
-        /// <typeparam name="TEvent">TEvent</typeparam>
-        /// <typeparam name="TEventHandler">TEventHandler</typeparam>
-        /// <returns>whether the operation success</returns>
-        public static bool Subscribe<TEvent, TEventHandler>(this IEventSubscriptionManager subscriptionManager)
-            where TEventHandler : class, IEventHandler<TEvent>
-            where TEvent : class, IEventBase
-        {
-            return subscriptionManager.Subscribe(typeof(TEvent), typeof(TEventHandler));
-        }
-
-        /// <summary>
-        /// add event handler for event
-        /// </summary>
-        /// <typeparam name="TEvent">TEvent</typeparam>
-        /// <typeparam name="TEventHandler">TEventHandler</typeparam>
-        /// <returns>whether the operation success</returns>
-        public static Task<bool> SubscribeAsync<TEvent, TEventHandler>(this IEventSubscriptionManager subscriptionManager)
-            where TEventHandler : class, IEventHandler<TEvent>
-            where TEvent : class, IEventBase
-        {
-            return subscriptionManager.SubscribeAsync(typeof(TEvent), typeof(TEventHandler));
-        }
-
-        /// <summary>
-        /// remove event handler for event
-        /// </summary>
-        /// <typeparam name="TEvent">TEvent</typeparam>
-        /// <typeparam name="TEventHandler">TEventHandler</typeparam>
-        /// <returns>whether the operation success</returns>
-        public static bool UnSubscribe<TEvent, TEventHandler>(this IEventSubscriptionManager subscriptionManager)
-            where TEventHandler : class, IEventHandler<TEvent>
-            where TEvent : class, IEventBase
-        {
-            return subscriptionManager.UnSubscribe(typeof(TEvent), typeof(TEventHandler));
-        }
-
-        /// <summary>
-        /// remove event handler for event
-        /// </summary>
-        /// <typeparam name="TEvent">TEvent</typeparam>
-        /// <typeparam name="TEventHandler">TEventHandler</typeparam>
-        /// <returns>whether the operation success</returns>
-        public static Task<bool> UnSubscribeAsync<TEvent, TEventHandler>(this IEventSubscriptionManager subscriptionManager)
-            where TEventHandler : class, IEventHandler<TEvent>
-            where TEvent : class, IEventBase
-        {
-            return subscriptionManager.UnSubscribeAsync(typeof(TEvent), typeof(TEventHandler));
         }
     }
 }
