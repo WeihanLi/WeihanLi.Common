2093baa2d382c244bf3d3c70016a7fb9976560e2
weihanli <weihanli@outlook.com>
2020-05-18 18:14:23 +0800

    add GetUserId with default value

--- src/WeihanLi.Common/Services/IUserIdProvider.cs
+++ src/WeihanLi.Common/Services/IUserIdProvider.cs
@@ -13,5 +13,10 @@ namespace WeihanLi.Common.Services
         {
             return userIdProvider.GetUserId().To<T>();
         }
+
+        public static T GetUserId<T>(this IUserIdProvider userIdProvider, T defaultValue)
+        {
+            return userIdProvider.GetUserId().ToOrDefault(defaultValue);
+        }
     }
 }
