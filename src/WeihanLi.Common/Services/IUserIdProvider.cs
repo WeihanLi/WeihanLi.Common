685a02bf3927d7b1d4424015d5aafb65b5a5cc74
weihanli <weihanli@outlook.com>
2020-05-18 18:06:22 +0800

    add IUserIdProvider.cs

---
+++ src/WeihanLi.Common/Services/IUserIdProvider.cs
@@ -0,0 +1,17 @@
+﻿using WeihanLi.Extensions;
+
+namespace WeihanLi.Common.Services
+{
+    public interface IUserIdProvider
+    {
+        string GetUserId();
+    }
+
+    public static class UserIdProviderExtensions
+    {
+        public static T GetUserId<T>(this IUserIdProvider userIdProvider)
+        {
+            return userIdProvider.GetUserId().To<T>();
+        }
+    }
+}
