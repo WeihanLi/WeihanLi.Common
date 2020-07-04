671d43b5b77bce92edc497a13dbdd361542a08e5
weihanli <weihanli@outlook.com>
2020-07-05 00:55:20 +0800

    add CommandRunner

---
+++ test/WeihanLi.Common.Test/HelpersTest/CommandRunnerTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using WeihanLi.Common.Helpers;
+using Xunit;
+
+namespace WeihanLi.Common.Test.HelpersTest
+{
+    public class CommandRunnerTest
+    {
+        [Fact]
+        public void HostNameTest()
+        {
+            var result = CommandRunner.ExecuteAndCapture("hostname");
+
+            var hostName = Dns.GetHostName();
+            Assert.Equal(hostName, result.StandardOut.TrimEnd());
+            Assert.Equal(0, result.ExitCode);
+        }
+    }
+}
