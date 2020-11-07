d3997d1f11a04554c5f86d4c995a45207c6bf41a
weihanli <weihanli@outlook.com>
2020-11-07 23:07:05 +0800

    exclude models

--- src/WeihanLi.Common/Helpers/NewFuncHelper.cs
+++ src/WeihanLi.Common/Helpers/NewFuncHelper.cs
@@ -5,6 +5,10 @@ namespace WeihanLi.Common.Helpers
 {
     public static class NewFuncHelper<T>
     {
+        /// <summary>
+        /// CreateNewInstance func
+        /// T need to have a parameter less constructor
+        /// </summary>
         public static readonly Func<T> Instance = Expression.Lambda<Func<T>>
         (
             Expression.New(typeof(T))
