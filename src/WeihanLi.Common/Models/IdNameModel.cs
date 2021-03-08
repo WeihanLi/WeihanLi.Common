6c1d8dcb1bc883cd6dafa1a6ab430dbdb1a1d6c8
weihanli <weihanli@outlook.com>
2021-03-08 23:28:38 +0800

    update models

---
+++ src/WeihanLi.Common/Models/IdNameModel.cs
@@ -0,0 +1,17 @@
+﻿namespace WeihanLi.Common.Models
+{
+    public class IdNameModel<TKey>
+    {
+        public TKey Id { get; set; } = default!;
+        public string Name { get; set; } = null!;
+    }
+
+    public class IdNameDescModel<TKey> : IdNameModel<TKey>
+    {
+        public string? Description { get; set; }
+    }
+
+    public class IdNameModel : IdNameModel<int> { }
+
+    public class IdNameDescModel : IdNameDescModel<int> { }
+}
