e50634b7185e4b80f43500596e5cf505c1ad1047
weihanli <weihanli@outlook.com>
2021-03-14 04:59:28 +0800

    update IdNameModel, add EnumHelper

--- src/WeihanLi.Common/Models/IdNameModel.cs
+++ src/WeihanLi.Common/Models/IdNameModel.cs
@@ -4,11 +4,24 @@
     {
         public TKey Id { get; set; } = default!;
         public string Name { get; set; } = null!;
+
+        public void Deconstruct(out TKey id, out string name)
+        {
+            id = Id;
+            name = Name;
+        }
     }
 
     public class IdNameDescModel<TKey> : IdNameModel<TKey>
     {
         public string? Description { get; set; }
+
+        public void Deconstruct(out TKey id, out string name, out string? description)
+        {
+            id = Id;
+            name = Name;
+            description = Description;
+        }
     }
 
     public class IdNameModel : IdNameModel<int> { }
---
+++ test/WeihanLi.Common.Test/HelpersTest/EnumHelperTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using WeihanLi.Common.Helpers;
+using WeihanLi.Common.Models;
+using Xunit;
+
+namespace WeihanLi.Common.Test.HelpersTest
+{
+    public class EnumHelperTest
+    {
+        [Fact]
+        public void IdNameListTest()
+        {
+            var list = EnumHelper.ToIdNameList<ReviewState>();
+            Assert.Equal(list.Count, Enum.GetNames<ReviewState>().Length);
+            foreach (var (id, name) in list)
+            {
+                Assert.True(Enum.TryParse(name, out ReviewState state));
+                Assert.Equal((int)state, id);
+            }
+        }
+
+        [Fact]
+        public void IdNameDescListTest()
+        {
+            var list = EnumHelper.ToIdNameDescList<ReviewState, sbyte>();
+            Assert.Equal(list.Count, Enum.GetNames<ReviewState>().Length);
+            foreach (var (id, name, description) in list)
+            {
+                Assert.True(Enum.TryParse(name, out ReviewState state));
+                Assert.Equal((int)state, id);
+                Assert.NotNull(description);
+            }
+        }
+    }
+}
