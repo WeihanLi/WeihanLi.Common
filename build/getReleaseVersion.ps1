bc36a8b6f56b91ce2682a920d6472e1543fc42ef
weihanli <weihanli@outlook.com>
2021-03-20 18:50:45 +0800

    update export env way

--- build/getReleaseVersion.ps1
+++ build/getReleaseVersion.ps1
@@ -3,6 +3,4 @@ $versionXml=([xml](Get-Content $versionPath))
 $versionProperty=$versionXml.Project.PropertyGroup
 $ReleaseVersion=$versionProperty.VersionMajor+"."+$versionProperty.VersionMinor+"."+$versionProperty.VersionPatch
 $ReleaseVersion
-echo "ReleaseVersion=${ReleaseVersion}" >> $Env.GITHUB_ENV
-echo "ReleaseVersion1=${ReleaseVersion}" >> $Env.$GITHUB_ENV
-echo "ReleaseVersion2=${ReleaseVersion}" >> $GITHUB_ENV
+Add-Content -Path $env:GITHUB_ENV -Value "ReleaseVersion=${ReleaseVersion}"
