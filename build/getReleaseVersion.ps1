d07123569bb29baa78d3b1a8d7bee450699cdf4f
weihanli <weihanli@outlook.com>
2021-03-20 17:30:56 +0800

    update getReleaseVersion script

--- build/getReleaseVersion.ps1
+++ build/getReleaseVersion.ps1
@@ -1,4 +1,4 @@
-$versionPath=$PWD.Path.EndsWith("build")?"version.props":"build/version.props"
+$versionPath=$PSScriptRoot+"/version.props"
 $versionXml=([xml](Get-Content $versionPath))
 $versionProperty=$versionXml.Project.PropertyGroup
 $env:ReleaseVersion=$versionProperty.VersionMajor+"."+$versionProperty.VersionMinor+"."+$versionProperty.VersionPatch
