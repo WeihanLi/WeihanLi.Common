fe9a421f52cc917743aa5255651d60934182dce6
weihanli <weihanli@outlook.com>
2021-03-20 18:43:09 +0800

    test export env var

--- build/getReleaseVersion.ps1
+++ build/getReleaseVersion.ps1
@@ -4,3 +4,5 @@ $versionProperty=$versionXml.Project.PropertyGroup
 $ReleaseVersion=$versionProperty.VersionMajor+"."+$versionProperty.VersionMinor+"."+$versionProperty.VersionPatch
 $ReleaseVersion
 Write-Output "ReleaseVersion=${ReleaseVersion}" >> $Env.GITHUB_ENV
+Write-Output "ReleaseVersion1=${ReleaseVersion}" >> $Env.$GITHUB_ENV
+Write-Output "ReleaseVersion2=${ReleaseVersion}" >> $GITHUB_ENV
