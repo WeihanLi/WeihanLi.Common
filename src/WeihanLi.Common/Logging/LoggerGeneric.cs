181a3cc564d39078557f06f195c4bdf249c826c5
Weihan Li <weihanli@outlook.com>
2021-04-30 15:28:43 +0800

    Update LoggerGeneric.cs

--- src/WeihanLi.Common/Logging/LoggerGeneric.cs
+++ src/WeihanLi.Common/Logging/LoggerGeneric.cs
@@ -47,11 +47,5 @@ namespace WeihanLi.Common.Logging
         {
             _logger.Log(logLevel, eventId, state, exception, formatter);
         }
-
-        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter) => throw new NotImplementedException();
-
-        public bool IsEnabled(LogLevel logLevel) => throw new NotImplementedException();
-
-        public IDisposable BeginScope<TState>(TState state) => throw new NotImplementedException();
     }
 }
