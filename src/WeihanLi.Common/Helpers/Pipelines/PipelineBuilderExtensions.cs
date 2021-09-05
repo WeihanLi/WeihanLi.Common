bb99d05b620ee99b6fff95b80f3db0e365312890
weihanli <weihanli@outlook.com>
2021-09-05 10:46:40 +0800

    Add ValueAsyncPipelineBuilder, fixes #122

--- src/WeihanLi.Common/Helpers/Pipelines/PipelineBuilderExtensions.cs
+++ src/WeihanLi.Common/Helpers/Pipelines/PipelineBuilderExtensions.cs
@@ -6,6 +6,7 @@ namespace WeihanLi.Common.Helpers
 {
     public static class PipelineBuilderExtensions
     {
+        #region IPipelineBuilder
         public static IPipelineBuilder<TContext> Use<TContext>(this IPipelineBuilder<TContext> builder, Action<TContext, Action> action)
 
         {
@@ -23,23 +24,6 @@ namespace WeihanLi.Common.Helpers
                 {
                     action(context, next);
                 });
-        }
-
-        public static IAsyncPipelineBuilder<TContext> Use<TContext>(this IAsyncPipelineBuilder<TContext> builder,
-            Func<TContext, Func<Task>, Task> func)
-        {
-            return builder.Use(next =>
-                context =>
-                {
-                    return func(context, () => next(context));
-                });
-        }
-
-        public static IAsyncPipelineBuilder<TContext> Use<TContext>(this IAsyncPipelineBuilder<TContext> builder,
-            Func<TContext, Func<TContext, Task>, Task> func)
-        {
-            return builder.Use(next =>
-                context => func(context, next));
         }
 
         public static IPipelineBuilder<TContext> Run<TContext>(this IPipelineBuilder<TContext> builder, Action<TContext> handler)
@@ -67,6 +51,26 @@ namespace WeihanLi.Common.Helpers
             return builder;
         }
 
+        #endregion
+        #region IAsyncPipelineBuilder
+
+        public static IAsyncPipelineBuilder<TContext> Use<TContext>(this IAsyncPipelineBuilder<TContext> builder,
+            Func<TContext, Func<Task>, Task> func)
+        {
+            return builder.Use(next =>
+                context =>
+                {
+                    return func(context, () => next(context));
+                });
+        }
+
+        public static IAsyncPipelineBuilder<TContext> Use<TContext>(this IAsyncPipelineBuilder<TContext> builder,
+            Func<TContext, Func<TContext, Task>, Task> func)
+        {
+            return builder.Use(next =>
+                context => func(context, next));
+        }
+
         public static IAsyncPipelineBuilder<TContext> When<TContext>(this IAsyncPipelineBuilder<TContext> builder, Func<TContext, bool> predict, Action<IAsyncPipelineBuilder<TContext>> configureAction)
         {
             builder.Use((context, next) =>
@@ -89,5 +93,53 @@ namespace WeihanLi.Common.Helpers
         {
             return builder.Use(_ => handler);
         }
+
+        #endregion
+#if NETSTANDARD2_1_OR_GREATER
+
+        #region IValueAsyncPipelineBuilder
+
+        public static IValueAsyncPipelineBuilder<TContext> Use<TContext>(this IValueAsyncPipelineBuilder<TContext> builder,
+            Func<TContext, Func<ValueTask>, ValueTask> func)
+        {
+            return builder.Use(next =>
+                context =>
+                {
+                    return func(context, () => next(context));
+                });
+        }
+
+        public static IValueAsyncPipelineBuilder<TContext> Use<TContext>(this IValueAsyncPipelineBuilder<TContext> builder,
+            Func<TContext, Func<TContext, ValueTask>, ValueTask> func)
+        {
+            return builder.Use(next =>
+                context => func(context, next));
+        }
+
+        public static IValueAsyncPipelineBuilder<TContext> When<TContext>(this IValueAsyncPipelineBuilder<TContext> builder, Func<TContext, bool> predict, Action<IValueAsyncPipelineBuilder<TContext>> configureAction)
+        {
+            builder.Use((context, next) =>
+            {
+                if (predict.Invoke(context))
+                {
+                    var branchPipelineBuilder = builder.New();
+                    configureAction(branchPipelineBuilder);
+                    var branchPipeline = branchPipelineBuilder.Build();
+                    return branchPipeline.Invoke(context);
+                }
+
+                return next();
+            });
+
+            return builder;
+        }
+
+        public static IValueAsyncPipelineBuilder<TContext> Run<TContext>(this IValueAsyncPipelineBuilder<TContext> builder, Func<TContext, ValueTask> handler)
+        {
+            return builder.Use(_ => handler);
+        }
+
+        #endregion  
+#endif
     }
 }
---
+++ src/WeihanLi.Common/Helpers/Pipelines/ValueAsyncPipelineBuilder.cs
@@ -0,0 +1,47 @@
+﻿#if NETSTANDARD2_1
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+// ReSharper disable once CheckNamespace
+namespace WeihanLi.Common.Helpers
+{
+    public interface IValueAsyncPipelineBuilder<TContext>
+    {
+        IValueAsyncPipelineBuilder<TContext> Use(Func<Func<TContext, ValueTask>, Func<TContext, ValueTask>> middleware);
+
+        Func<TContext, ValueTask> Build();
+
+        IValueAsyncPipelineBuilder<TContext> New();
+    }
+
+    internal sealed class ValueAsyncPipelineBuilder<TContext> : IValueAsyncPipelineBuilder<TContext>
+    {
+        private readonly Func<TContext, ValueTask> _completeFunc;
+        private readonly List<Func<Func<TContext, ValueTask>, Func<TContext, ValueTask>>> _pipelines = new();
+
+        public ValueAsyncPipelineBuilder(Func<TContext, ValueTask> completeFunc)
+        {
+            _completeFunc = completeFunc;
+        }
+
+        public IValueAsyncPipelineBuilder<TContext> Use(Func<Func<TContext, ValueTask>, Func<TContext, ValueTask>> middleware)
+        {
+            _pipelines.Add(middleware);
+            return this;
+        }
+
+        public Func<TContext, ValueTask> Build()
+        {
+            var request = _completeFunc;
+            for (var i = _pipelines.Count - 1; i >= 0; i--)
+            {
+                request = _pipelines[i](request);
+            }
+            return request;
+        }
+
+        public IValueAsyncPipelineBuilder<TContext> New() => new ValueAsyncPipelineBuilder<TContext>(_completeFunc);
+    }
+}
+#endif
