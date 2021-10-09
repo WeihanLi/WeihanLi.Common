9dd204122fa2efa2db353c67e5aee33c2252e908
weihanli <weihanli@outlook.com>
2021-10-09 08:49:10 +0800

    Add TenantIdProvider, fixes #124

---
+++ src/WeihanLi.Common/Models/TenantInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace WeihanLi.Common.Models
+{
+    public class TenantInfo<TKey>
+    {
+        public TKey? TenantId { get; set; }
+
+        public string? TenantName { get; set; }
+    }
+
+    public class TenantInfo: TenantInfo<string> {}
+}
---
+++ src/WeihanLi.Common/Services/ITenantProvider.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using WeihanLi.Common.Models;
+using WeihanLi.Extensions;
+
+namespace WeihanLi.Common.Services
+{
+    public interface ITenantProvider
+    {
+        string GetTenantId();
+
+        TenantInfo<TKey> GetTenantInfo<TKey>();
+    }
+
+    public static class TenantIdProviderExtensions
+    {
+        public static T? GetTenantId<T>(this ITenantProvider tenantIdProvider, T? defaultValue = default)
+        {
+            return tenantIdProvider.GetTenantId().ToOrDefault(defaultValue);
+        }
+
+        public static bool TryGetTenantId<T>(this ITenantProvider tenantIdProvider, out T? value, T? defaultValue = default)
+        {
+            try
+            {
+                var tenantId = tenantIdProvider.GetTenantId();
+                if (!string.IsNullOrEmpty(tenantId))
+                {
+                    value = tenantId.To<T>();
+                    return true;
+                }
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
+
+            value = defaultValue;
+            return false;
+        }
+    }
+}
