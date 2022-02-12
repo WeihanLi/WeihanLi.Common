e15f79680f9a8e6c62e589f6b6817e12d685703e
Weihan Li <weihanli@outlook.com>
2022-02-12 22:31:02 +0800

    Add JsonHttpContent

---
+++ src/WeihanLi.Common/Http/JsonHttpContent.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Weihan Li. All rights reserved.
+// Licensed under the MIT license.
+
+using Newtonsoft.Json;
+using System.Text;
+
+namespace WeihanLi.Common.Http;
+
+public sealed class JsonHttpContent : StringContent
+{
+    public JsonHttpContent(object obj, JsonSerializerSettings? jsonSerializerSettings = null)
+        : this(JsonConvert.SerializeObject(obj, jsonSerializerSettings))
+    {
+    }
+
+    public JsonHttpContent(string content) : this(content, Encoding.UTF8)
+    {
+    }
+
+    public JsonHttpContent(string content, Encoding encoding) : base(content, encoding, "application/json")
+    {
+    }
+}
