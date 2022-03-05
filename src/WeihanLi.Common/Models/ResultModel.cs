350bc161bb53c576f5edea8941e1a3994adf1526
Weihan Li <weihanli@outlook.com>
2022-03-05 18:53:57 +0800

    Update Result

--- src/WeihanLi.Common/Models/ResultModel.cs
+++ src/WeihanLi.Common/Models/ResultModel.cs
@@ -1,10 +1,9 @@
-﻿using System.ComponentModel;
-
-namespace WeihanLi.Common.Models;
+﻿namespace WeihanLi.Common.Models;
 
 /// <summary>
 /// ResultModel
 /// </summary>
+[Obsolete("Please use ")]
 public record ResultModel
 {
     /// <summary>
@@ -68,6 +67,7 @@ public record ResultModel
 /// ResultModel
 /// </summary>
 /// <typeparam name="T">Type</typeparam>
+[Obsolete]
 public record ResultModel<T> : ResultModel
 {
     /// <summary>
@@ -84,59 +84,4 @@ public record ResultModel<T> : ResultModel
             ErrorMsg = ErrorMsg,
         };
     }
-}
-
-/// <summary>
-/// ResultStatus
-/// 返回的结果状态
-/// </summary>
-public enum ResultStatus
-{
-    [Description("Empty Status")]
-    None = 0,
-
-    [Description("Continue")]
-    Continue = 100,
-
-    [Description("Processing")]
-    Processing = 102,
-
-    [Description("Success")]
-    Success = 200,
-
-    [Description("Created")]
-    Created = 201,
-
-    [Description("Accepted")]
-    Accepted = 202,
-
-    [Description("BadRequest, Request Parameter Error")]
-    RequestError = 400,
-
-    [Description("Unauthorized")]
-    Unauthorized = 401,
-
-    [Description("No permission")]
-    NoPermission = 403,
-
-    [Description("ResourceNotFound")]
-    ResourceNotFound = 404,
-
-    [Description("MethodNotAllowed")]
-    MethodNotAllowed = 405,
-
-    [Description("RequestTimeout")]
-    RequestTimeout = 408,
-
-    [Description("ProcessFail,Server Internal Error")]
-    ProcessFail = 500,
-
-    [Description("Not Implemented")]
-    NotImplemented = 501,
-
-    [Description("ServiceUnavailable")]
-    ServiceUnavailable = 503,
-
-    [Description("VersionNotSupported")]
-    VersionNotSupported = 505
 }
---
+++ src/WeihanLi.Common/Models/ResultStatus.cs
@@ -0,0 +1,60 @@
+﻿// Copyright (c) Weihan Li. All rights reserved.
+// Licensed under the Apache license.
+
+using System.ComponentModel;
+
+namespace WeihanLi.Common.Models;
+
+/// <summary>
+/// Result Status
+/// </summary>
+public enum ResultStatus
+{
+    [Description("Empty Status")]
+    None = 0,
+
+    [Description("Continue")]
+    Continue = 100,
+
+    [Description("Processing")]
+    Processing = 102,
+
+    [Description("Success")]
+    Success = 200,
+
+    [Description("Created")]
+    Created = 201,
+
+    [Description("Accepted")]
+    Accepted = 202,
+
+    [Description("BadRequest, Request Parameter Error")]
+    RequestError = 400,
+
+    [Description("Unauthorized")]
+    Unauthorized = 401,
+
+    [Description("NoPermission")]
+    NoPermission = 403,
+
+    [Description("ResourceNotFound")]
+    ResourceNotFound = 404,
+
+    [Description("MethodNotAllowed")]
+    MethodNotAllowed = 405,
+
+    [Description("RequestTimeout")]
+    RequestTimeout = 408,
+
+    [Description("Process failed, Server Internal Error")]
+    ProcessFail = 500,
+
+    [Description("Not Implemented")]
+    NotImplemented = 501,
+
+    [Description("ServiceUnavailable")]
+    ServiceUnavailable = 503,
+
+    [Description("VersionNotSupported")]
+    VersionNotSupported = 505
+}
