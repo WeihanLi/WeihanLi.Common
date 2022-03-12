65abc10faad042d45609ebdf4a80b167906436a5
Weihan Li <weihanli@outlook.com>
2022-03-12 16:06:46 +0800

    Update Guard to use NullableAttribute

--- src/WeihanLi.Common/Guard.cs
+++ src/WeihanLi.Common/Guard.cs
@@ -1,21 +1,30 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
 
 namespace WeihanLi.Common;
 
 public static class Guard
 {
-    public static T NotNull<T>(T? t,
+    public static T NotNull<T>([NotNull] T? t,
            [CallerArgumentExpression("t")]
             string? paramName = default)
     {
+#if NET6_0_OR_GREATER
+        ArgumentNullException.ThrowIfNull(t, paramName);
+#else
         if (t is null)
         {
-            throw new ArgumentNullException(paramName);
+            ThrowNullException(paramName);
         }
+#endif
         return t;
     }
 
-    public static string NotNullOrEmpty(string? str,
+    [DoesNotReturn]
+    private static void ThrowNullException(string? paramName) =>
+        throw new ArgumentNullException(paramName);
+
+    public static string NotNullOrEmpty([NotNull] string? str,
         [CallerArgumentExpression("str")]
             string? paramName = default)
     {
@@ -24,6 +33,6 @@ public static class Guard
         {
             throw new ArgumentException("The argument is IsNullOrEmpty", paramName);
         }
-        return str!;
+        return str;
     }
 }
