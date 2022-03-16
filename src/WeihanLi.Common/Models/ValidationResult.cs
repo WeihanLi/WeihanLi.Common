00fbdd44629b59fbbb87f9695557dcb563a2c812
Weihan Li <weihanli@outlook.com>
2022-03-17 00:09:57 +0800

    Update ValidationResult

---
+++ src/WeihanLi.Common/Models/ValidationResult.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Weihan Li. All rights reserved.
+// Licensed under the Apache license.
+
+namespace WeihanLi.Common.Models;
+
+public class ValidationResult
+{
+    /// <summary>
+    /// Valid
+    /// </summary>
+    public bool Valid { get; set; }
+
+    /// <summary>
+    /// ErrorMessages
+    /// Key: memberName
+    /// Value: errorMessages
+    /// </summary>
+    public Dictionary<string, string[]>? Errors { get; set; }
+}
---
+++ src/WeihanLi.Common/Services/Validator.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WeihanLi.Extensions;
+using AnnotationValidationResult = System.ComponentModel.DataAnnotations.ValidationResult;
+using ValidationResult = WeihanLi.Common.Models.ValidationResult;
+
+namespace WeihanLi.Common.Services;
+
+public interface IValidator
+{
+    ValidationResult Validate(object value);
+}
+
+public class DataAnnotationValidator : IValidator
+{
+    public virtual ValidationResult Validate(object value)
+    {
+        var validationResult = new ValidationResult();
+        var annotationValidateResults = new List<AnnotationValidationResult>();
+        validationResult.Valid =
+            Validator.TryValidateObject(value, new ValidationContext(value), annotationValidateResults);
+        validationResult.Errors = annotationValidateResults
+            .GroupBy(x => x.MemberNames.StringJoin(","))
+            .ToDictionary(g => g.Key, g => g.Select(x => x.ErrorMessage).WhereNotNull().ToArray());
+        return validationResult;
+    }
+}
+
+public sealed class DelegateValidator : IValidator
+{
+    private readonly Func<object, ValidationResult> _validateFunc;
+
+    public DelegateValidator(Func<object, ValidationResult> validateFunc)
+    {
+        _validateFunc = Guard.NotNull(validateFunc);
+    }
+
+    public ValidationResult Validate(object value)
+    {
+        return _validateFunc.Invoke(value);
+    }
+}
