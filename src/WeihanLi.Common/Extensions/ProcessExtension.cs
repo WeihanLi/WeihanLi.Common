87d9d80baa4f7ac98a6a0c54308b8c63e28d9568
Weihan Li <weihanli@outlook.com>
2022-07-09 10:21:43 +0800

    Refine ProcessExtension

--- src/WeihanLi.Common/Extensions/ProcessExtension.cs
+++ src/WeihanLi.Common/Extensions/ProcessExtension.cs
@@ -17,12 +17,18 @@ public static class ProcessExtension
     public static Task WaitForExitAsync(this Process process, CancellationToken cancellationToken = default)
     {
         Guard.NotNull(process);
-
+        process.EnableRaisingEvents = true;
         var tcs = new TaskCompletionSource<object?>(TaskCreationOptions.RunContinuationsAsynchronously);
-        EventHandler handler = (_, _) => tcs.TrySetResult(null);
+        void EventHandler(object o, EventArgs eventArgs) => tcs.TrySetResult(null);
         try
         {
-            process.Exited += handler;
+            process.Exited += EventHandler;
+
+            if (process.StartTime == DateTime.MinValue)
+            {
+                process.Start();
+            }
+
             tcs.Task.Wait(cancellationToken);
         }
         catch (OperationCanceledException) when (cancellationToken.IsCancellationRequested)
@@ -35,7 +41,7 @@ public static class ProcessExtension
         }
         finally
         {
-            process.Exited -= handler;
+            process.Exited -= EventHandler;
         }
 
         return tcs.Task;
@@ -43,17 +49,66 @@ public static class ProcessExtension
 #endif
 
     /// <summary>
+    /// Execute command with a process
+    /// </summary>
+    /// <param name="processStartInfo">processStartInfo</param>
+    /// <returns>process exit code</returns>
+    public static int Execute(this ProcessStartInfo processStartInfo)
+    {
+        using var process = new Process()
+        {
+            StartInfo = processStartInfo
+        };
+        process.Start();
+        process.WaitForExit();
+        return process.ExitCode;
+    }
+
+    /// <summary>
+    /// Execute command with a process
+    /// </summary>
+    /// <param name="processStartInfo">processStartInfo</param>
+    /// <param name="cancellationToken">cancellationToken</param>
+    /// <returns>process exit code</returns>
+    public static async Task<int> ExecuteAsync(this ProcessStartInfo processStartInfo, CancellationToken cancellationToken = default)
+    {
+        using var process = new Process()
+        {
+            StartInfo = processStartInfo
+        };
+        process.Start();
+        await process.WaitForExitAsync(cancellationToken);
+        return process.ExitCode;
+    }
+
+    /// <summary>
     /// Get process execute result
     /// </summary>
     /// <param name="psi">ProcessStartInfo</param>
     /// <returns>process output and exitCode</returns>
-    public static async Task<CommandResult> GetResultAsync(this ProcessStartInfo psi)
+    public static CommandResult GetResult(this ProcessStartInfo psi)
     {
         var stdOutStringBuilder = new StringBuilder();
         using var stdOut = new StringWriter(stdOutStringBuilder);
         var stdErrStringBuilder = new StringBuilder();
         using var stdErr = new StringWriter(stdErrStringBuilder);
-        var exitCode = await GetExitCodeAsync(psi, stdOut, stdErr);
+        var exitCode = GetExitCode(psi, stdOut, stdErr);
+        return new CommandResult(exitCode, stdOutStringBuilder.ToString(), stdErrStringBuilder.ToString());
+    }
+
+    /// <summary>
+    /// Get process execute result
+    /// </summary>
+    /// <param name="psi">ProcessStartInfo</param>
+    /// <param name="cancellationToken">cancellationToken</param>
+    /// <returns>process output and exitCode</returns>
+    public static async Task<CommandResult> GetResultAsync(this ProcessStartInfo psi, CancellationToken cancellationToken = default)
+    {
+        var stdOutStringBuilder = new StringBuilder();
+        using var stdOut = new StringWriter(stdOutStringBuilder);
+        var stdErrStringBuilder = new StringBuilder();
+        using var stdErr = new StringWriter(stdErrStringBuilder);
+        var exitCode = await GetExitCodeAsync(psi, stdOut, stdErr, cancellationToken);
         return new(exitCode, stdOutStringBuilder.ToString(), stdErrStringBuilder.ToString());
     }
 
@@ -64,8 +119,51 @@ public static class ProcessExtension
     /// <param name="stdOut">Defaults to Console.Out</param>
     /// <param name="stdErr">Defaults to Console.Error</param>
     /// <returns>Process exit code</returns>
-    public static async Task<int> GetExitCodeAsync(this ProcessStartInfo psi, TextWriter? stdOut = null,
+    public static int GetExitCode(this ProcessStartInfo psi, TextWriter? stdOut = null,
         TextWriter? stdErr = null)
+    {
+        stdOut ??= Console.Out;
+        stdErr ??= Console.Error;
+        psi.UseShellExecute = false;
+        psi.RedirectStandardError = true;
+        psi.RedirectStandardOutput = true;
+        using var process = new Process { StartInfo = psi };
+        process.OutputDataReceived += (_, e) =>
+        {
+            if (e.Data != null)
+                stdOut.WriteLine(e.Data);
+        };
+        process.ErrorDataReceived += (_, e) =>
+        {
+            if (e.Data != null)
+                stdErr.WriteLine(e.Data);
+        };
+        try
+        {
+            process.Start();
+        }
+        catch (Win32Exception win32Exception)
+        {
+            return win32Exception.ErrorCode;
+        }
+
+        process.BeginOutputReadLine();
+        process.BeginErrorReadLine();
+        process.WaitForExit();
+
+        return process.ExitCode;
+    }
+
+    /// <summary>
+    /// Wait for process exit and get exit code
+    /// </summary>
+    /// <param name="psi">Process is started from this information</param>
+    /// <param name="stdOut">Defaults to Console.Out</param>
+    /// <param name="stdErr">Defaults to Console.Error</param>
+    /// <param name="cancellationToken">cancellationToken</param>
+    /// <returns>Process exit code</returns>
+    public static async Task<int> GetExitCodeAsync(this ProcessStartInfo psi, TextWriter? stdOut = null,
+        TextWriter? stdErr = null, CancellationToken cancellationToken = default)
     {
         stdOut ??= Console.Out;
         stdErr ??= Console.Error;
@@ -100,7 +198,7 @@ public static class ProcessExtension
 
         process.BeginOutputReadLine();
         process.BeginErrorReadLine();
-        await Task.WhenAll(process.WaitForExitAsync(), stdOutComplete.Task, stdErrComplete.Task);
+        await Task.WhenAll(process.WaitForExitAsync(cancellationToken), stdOutComplete.Task, stdErrComplete.Task);
 
         return process.ExitCode;
     }
--- src/WeihanLi.Common/Helpers/CommandExecutor.cs
+++ src/WeihanLi.Common/Helpers/CommandExecutor.cs
@@ -1,22 +1,13 @@
-﻿using System.Diagnostics;
+﻿// Copyright (c) Weihan Li. All rights reserved.
+// Licensed under the Apache license.
+
+using System.Diagnostics;
+using WeihanLi.Extensions;
 
 namespace WeihanLi.Common.Helpers;
 
 public static class CommandExecutor
 {
-    private static bool TryKill(this Process process)
-    {
-        try
-        {
-            process.Kill();
-            return true;
-        }
-        catch
-        {
-            return false;
-        }
-    }
-
     /// <summary>
     /// Execute command with a process
     /// </summary>
@@ -26,65 +17,31 @@ public static class CommandExecutor
     /// <returns>exit code</returns>
     public static int Execute(string commandPath, string? arguments = null, string? workingDirectory = null)
     {
-        return Execute(new ProcessStartInfo(commandPath, arguments ?? string.Empty)
+        return new ProcessStartInfo(commandPath, arguments ?? string.Empty)
         {
             UseShellExecute = false,
             CreateNoWindow = true,
 
             WorkingDirectory = workingDirectory ?? Environment.CurrentDirectory
-        });
+        }.Execute();
     }
 
     /// <summary>
     /// Execute command with a process
     /// </summary>
-    /// <param name="processStartInfo">processStartInfo</param>
-    /// <returns>process exit code</returns>
-    public static int Execute(this ProcessStartInfo processStartInfo)
+    /// <param name="commandPath">executable command path</param>
+    /// <param name="arguments">command arguments</param>
+    /// <param name="workingDirectory">working directory</param>
+    /// <returns>exit code</returns>
+    public static async Task<int> ExecuteAsync(string commandPath, string? arguments = null, string? workingDirectory = null)
     {
-        using var process = new Process()
+        return await new ProcessStartInfo(commandPath, arguments ?? string.Empty)
         {
-            StartInfo = processStartInfo
-        };
-        process.Start();
-        process.WaitForExit();
-        return process.ExitCode;
-    }
+            UseShellExecute = false,
+            CreateNoWindow = true,
 
-
-    /// <summary>
-    /// Execute command with a process
-    /// </summary>
-    /// <param name="processStartInfo">processStartInfo</param>
-    /// <param name="cancellationToken">cancellationToken</param>
-    /// <returns>process exit code</returns>
-    public static async Task<int> ExecuteAsync(this ProcessStartInfo processStartInfo, CancellationToken cancellationToken = default)
-    {
-        using var process = new Process()
-        {
-            StartInfo = processStartInfo
-        };
-
-        var tcs = new TaskCompletionSource<object>(TaskCreationOptions.RunContinuationsAsynchronously);
-
-        process.EnableRaisingEvents = true;
-        process.Exited += (s, e) => tcs.TrySetResult(0);
-
-        process.Start();
-
-        using (cancellationToken.Register(
-                   () =>
-                   {
-                       if (process.TryKill())
-                       {
-                           _ = tcs.TrySetCanceled(cancellationToken);
-                       }
-                   }, false))
-        {
-            _ = await tcs.Task.ConfigureAwait(false);
-        }
-
-        return process.ExitCode;
+            WorkingDirectory = workingDirectory ?? Environment.CurrentDirectory
+        }.ExecuteAsync();
     }
 
     /// <summary>
@@ -112,23 +69,8 @@ public static class CommandExecutor
     /// <returns>command execute result</returns>
     public static CommandResult ExecuteAndCapture(this ProcessStartInfo processStartInfo)
     {
-        // ensure output redirect
-        processStartInfo.RedirectStandardOutput = true;
-        processStartInfo.RedirectStandardError = true;
-        // create a new process to execute command
-        using var process = new Process()
-        {
-            StartInfo = processStartInfo
-        };
-        process.Start();
-
-        var standardOut = process.StandardOutput.ReadToEnd();
-        var standardError = process.StandardError.ReadToEnd();
-
-        process.WaitForExit();
-        return new CommandResult(process.ExitCode, standardOut, standardError);
+        return processStartInfo.GetResult();
     }
-
 
     /// <summary>
     /// Execute command with a process and capture the output
@@ -156,35 +98,7 @@ public static class CommandExecutor
     /// <returns>command execute result</returns>
     public static async Task<CommandResult> ExecuteAndCaptureAsync(this ProcessStartInfo processStartInfo, CancellationToken cancellationToken = default)
     {
-        // ensure output redirect
-        processStartInfo.RedirectStandardOutput = true;
-        processStartInfo.RedirectStandardError = true;
-        // create a new process to execute command
-        using var process = new Process()
-        {
-            StartInfo = processStartInfo
-        };
-        var tcs = new TaskCompletionSource<object>(TaskCreationOptions.RunContinuationsAsynchronously);
-
-        process.EnableRaisingEvents = true;
-        process.Exited += (s, e) => tcs.TrySetResult(0);
-        process.Start();
-
-        var standardOutTask = process.StandardOutput.ReadToEndAsync();
-        var standardErrorTask = process.StandardError.ReadToEndAsync();
-
-        using (cancellationToken.Register(
-                   () =>
-                   {
-                       if (process.TryKill())
-                       {
-                           _ = tcs.TrySetCanceled(cancellationToken);
-                       }
-                   }, false))
-        {
-            await Task.WhenAll(standardOutTask, standardErrorTask, tcs.Task).ConfigureAwait(false);
-            return new CommandResult(process.ExitCode, standardOutTask.Result, standardErrorTask.Result);
-        }
+        return await processStartInfo.GetResultAsync(cancellationToken);
     }
 }
 
