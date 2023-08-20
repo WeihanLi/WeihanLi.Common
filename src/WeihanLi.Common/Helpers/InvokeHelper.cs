247f433e2afedc8ed188bfeb95e7bd04e651f646
Weihan Li <weihanli@outlook.com>
2023-08-20 20:51:00 +0800

    feat: update InvokeExitHandler

--- src/WeihanLi.Common/Helpers/InvokeHelper.cs
+++ src/WeihanLi.Common/Helpers/InvokeHelper.cs
@@ -102,10 +102,19 @@ public static class InvokeHelper
     private static void InvokeExitHandler(object? sender, EventArgs? args)
     {
         if (_exited) return;
+        if (args is ConsoleCancelEventArgs consoleCancelEventArgs)
+        {
+            consoleCancelEventArgs.Cancel = true;
+        }
+#if NET6_0_OR_GREATER
+        if (sender is PosixSignalContext posixSignalContext)
+        {
+            posixSignalContext.Cancel = true;
+        }
+#endif
         lock (_exitLock)
         {
             if (_exited) return;
-
             Debug.WriteLine("exiting...");
             if (LazyCancellationTokenSource.IsValueCreated)
             {
