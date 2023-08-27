022ffc200c24199363372fde876f797a56b69d75
Weihan Li <weihanli@outlook.com>
2023-08-27 11:05:41 +0800

    Update commit-lint.cs
    add sample

--- .husky/scripts/commit-lint.cs
+++ .husky/scripts/commit-lint.cs
@@ -4,7 +4,7 @@ https://www.conventionalcommits.org/en/v1.0.0/
 https://github.com/angular/angular/blob/22b96b9/CONTRIBUTING.md#type
 */
 
-const string pattern = @"^(?=.{1,90}$)(?:build|feat|ci|chore|docs|fix|perf|refactor|revert|style|test)(?:\(.+\))*(?::).{4,}(?:#\d+)*(?<![\.\s])$";
+const string pattern = @"^(?=.{1,90}$)(?:build|feat|ci|chore|docs|fix|perf|refactor|revert|style|sample|test)(?:\(.+\))*(?::).{4,}(?:#\d+)*(?<![\.\s])$";
 
 var msg = File.ReadAllLines(args[0])[0];
 Console.WriteLine("Your commit headline message is:\n> {0}", msg);
