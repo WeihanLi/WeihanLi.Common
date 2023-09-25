5d920fb9f00a9fd389ef7663525638db41262185
Weihan Li <weihanli@outlook.com>
2023-09-25 22:26:32 +0800

    sample: update AppHostTest

--- samples/DotNetCoreSample/AppHostTest.cs
+++ samples/DotNetCoreSample/AppHostTest.cs
@@ -2,8 +2,10 @@
 // Licensed under the Apache license.
 
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Console;
+using System.Net;
 using System.Text.Encodings.Web;
 using System.Text.Json;
 using WeihanLi.Common.Helpers;
@@ -17,13 +19,12 @@ public static class AppHostTest
     {
         var builder = AppHost.CreateBuilder();
         builder.Configuration.AddJsonFile("appsettings.json");
-        builder.Logging.AddNewtonJsonConsole(options =>
-        {
-            options.TimestampFormat = "yyyy-MM-dd HH:mm:ss";
-        });
+        builder.Logging.AddRelaxedJsonConsole(options => { options.TimestampFormat = "yyyy-MM-dd HH:mm:ss"; });
         // builder.AddHostedService<TimerService>();
-        builder.AddHostedService<DiagnosticBackgroundService>();
-        var cts = new CancellationTokenSource(5000);
+        // builder.AddHostedService<DiagnosticBackgroundService>();
+        builder.Services.AddSingleton<IWebServer, HttpListenerWebServer>();
+        builder.AddHostedService<WebServerHostedService>();
+        var cts = new CancellationTokenSource(60_000);
         var app = builder.Build();
         await app.RunAsync(cts.Token);
     }
@@ -32,6 +33,7 @@ public static class AppHostTest
 file sealed class TimerService : TimerBaseBackgroundService
 {
     protected override TimeSpan Period => TimeSpan.FromSeconds(1);
+
     protected override Task TimedTask(CancellationToken cancellationToken)
     {
         Console.WriteLine(DateTimeOffset.Now);
@@ -46,6 +48,7 @@ file sealed class DiagnosticBackgroundService : CronBasedBackgroundServiceWithDi
     }
 
     protected override string CronExpression => CronHelper.Secondly;
+
     protected override Task TimedTask(IServiceProvider serviceProvider, CancellationToken cancellationToken)
     {
         Console.WriteLine(DateTimeOffset.Now);
@@ -55,7 +58,7 @@ file sealed class DiagnosticBackgroundService : CronBasedBackgroundServiceWithDi
 
 public static partial class LoggingBuilderExtensions
 {
-    public static ILoggingBuilder AddRelaxedJsonConsole(this ILoggingBuilder loggingBuilder, 
+    public static ILoggingBuilder AddRelaxedJsonConsole(this ILoggingBuilder loggingBuilder,
         Action<JsonConsoleFormatterOptions>? optionsConfigure = null)
     {
         return loggingBuilder.AddJsonConsole(options =>
@@ -66,5 +69,77 @@ public static partial class LoggingBuilderExtensions
             };
             optionsConfigure?.Invoke(options);
         });
+    }
+}
+
+file interface IWebServer
+{
+    Task StartAsync(CancellationToken cancellationToken);
+
+    Task StopAsync(CancellationToken cancellationToken);
+}
+
+file sealed class HttpListenerWebServer : IWebServer
+{
+    private readonly IServiceProvider _serviceProvider;
+    private readonly HttpListener _listener = new();
+
+    public HttpListenerWebServer(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        _listener.Prefixes.Add("http://localhost:5100/");
+        _listener.Start();
+        var logger = _serviceProvider.GetRequiredService<ILogger<HttpListenerWebServer>>();
+        logger.LogInformation("WebServer started");
+        
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            var listenerContext = await _listener.GetContextAsync();
+            try
+            {
+                await listenerContext.Response.OutputStream.WriteAsync("Hello World"u8.ToArray(), cancellationToken);
+            }
+            catch (Exception) when (!cancellationToken.IsCancellationRequested)
+            {
+                throw;
+            }
+            finally
+            {
+                listenerContext.Response.Close();
+            }
+        }
+
+        _listener.Stop();
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        _listener.Stop();
+        return Task.CompletedTask;
+    }
+}
+
+file sealed class WebServerHostedService : BackgroundService
+{
+    private readonly IWebServer _server;
+
+    public WebServerHostedService(IWebServer server)
+    {
+        _server = server;
+    }
+    
+    public override async Task StopAsync(CancellationToken cancellationToken)
+    {
+        await _server.StopAsync(cancellationToken);
+        await base.StopAsync(cancellationToken);
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        await _server.StartAsync(stoppingToken);
     }
 }
