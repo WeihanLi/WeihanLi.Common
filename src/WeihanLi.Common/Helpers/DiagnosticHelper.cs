a8f724ff05184255c90cc52fc7f47e6811b80a86
Weihan Li <weihanli@outlook.com>
2023-11-15 21:28:04 +0800

    feat: add WeihanLi.Extensions.Hosting.csproj

--- src/WeihanLi.Common/Helpers/DiagnosticHelper.cs
+++ src/WeihanLi.Common/Helpers/DiagnosticHelper.cs
@@ -9,8 +9,8 @@ namespace WeihanLi.Common.Helpers;
 public static class DiagnosticHelper
 {
     private const string DiagnosticSourceName = "WeihanLi.Common";
-    internal static readonly ActivitySource ActivitySource;
-    internal static readonly Meter Meter;
+    public static readonly ActivitySource ActivitySource;
+    public static readonly Meter Meter;
     static DiagnosticHelper()
     {
         var version = typeof(DiagnosticHelper).Assembly.GetName().Version?.ToString(3);
