1311d0f031a6f5ee8ed878468af762068c5b9592
Weihan Li <weihanli@outlook.com>
2024-01-04 14:25:25 +0800

    feat: update DefaultTemplateParser regex

--- test/WeihanLi.Common.Test/TemplateTest/TemplateParserTest.cs
+++ test/WeihanLi.Common.Test/TemplateTest/TemplateParserTest.cs
@@ -15,9 +15,18 @@ public class TemplateParserTest
                 Build Status: {{Status}}
                 Chart: {{$env CHART_NAME}} - {{$env VERSION}}
                 AppVersion: {{$env APP_VERSION}}
+                Config: {{$config AppSettings:Host}}
+                Config: {{$config AppSettings--Host}}
+                Config: {{$config AppSettings__Host}}
                 """;
         var result = await new DefaultTemplateParser()
             .ParseAsync(template);
-        Assert.Equal(4, result.Variables.Count);
+        Assert.Equal(6, result.Variables.Count);
+        Assert.Contains("Status", result.Variables);
+        Assert.Contains("$env CHART_NAME", result.Variables);
+        Assert.Contains("$env VERSION", result.Variables);
+        Assert.Contains("$env APP_VERSION", result.Variables);
+        Assert.Contains("$config AppSettings:Host", result.Variables);
+        Assert.Contains("$config AppSettings__Host", result.Variables);
     }
 }
