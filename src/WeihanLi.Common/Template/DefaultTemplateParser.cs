58a81b96fe655a79dc61ca410be4a20b2b5caa4b
Weihan Li <weihanli@outlook.com>
2024-01-04 13:11:47 +0800

    feat: fix DefaultTemplateParser regex

--- src/WeihanLi.Common/Template/DefaultTemplateParser.cs
+++ src/WeihanLi.Common/Template/DefaultTemplateParser.cs
@@ -7,19 +7,16 @@ namespace WeihanLi.Common.Template;
 
 internal sealed class DefaultTemplateParser : ITemplateParser
 {
-    private const string VariableRegexExp = @"\{\{(?<Variable>.+)\}\}";
+    private const string VariableRegexExp = @"\{\{(?<Variable>[\w\$\s]+)\}\}";
     private static readonly Regex VariableRegex = new(VariableRegexExp, RegexOptions.Compiled);
     public Task<TemplateRenderContext> ParseAsync(string text)
     {
         var variables = new HashSet<string>();
-        var match = VariableRegex.Match(text); ;
+        var match = VariableRegex.Match(text);
         while (match.Success)
         {
             var variable = match.Groups["Variable"].Value;
-            if (!string.IsNullOrEmpty(variable))
-            {
-                variables.Add(variable);
-            }
+            variables.Add(variable);
             match = match.NextMatch();
         }
         var context = new TemplateRenderContext(text, variables);
