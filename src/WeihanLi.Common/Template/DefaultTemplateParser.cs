3fa23ee2c8db94bfaa6e2d8f48ad4a699754124e
Weihan Li <weihanli@outlook.com>
2024-01-04 14:25:25 +0800

    feat: update DefaultTemplateParser regex

--- src/WeihanLi.Common/Template/DefaultTemplateParser.cs
+++ src/WeihanLi.Common/Template/DefaultTemplateParser.cs
@@ -7,7 +7,7 @@ namespace WeihanLi.Common.Template;
 
 internal sealed class DefaultTemplateParser : ITemplateParser
 {
-    private const string VariableRegexExp = @"\{\{(?<Variable>[\w\$\s]+)\}\}";
+    private const string VariableRegexExp = @"\{\{(?<Variable>[\w\$\s:]+)\}\}";
     private static readonly Regex VariableRegex = new(VariableRegexExp, RegexOptions.Compiled);
     public Task<TemplateRenderContext> ParseAsync(string text)
     {
