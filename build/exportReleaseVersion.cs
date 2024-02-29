91d0e9ec2a9f3e2989abc9e110163a902078baf5
Weihan Li <weihanli@outlook.com>
2024-02-29 23:58:10 +0800

    build: fix exportReleaseVersion path issue

--- build/exportReleaseVersion.cs
+++ build/exportReleaseVersion.cs
@@ -1,12 +1,17 @@
-var versionPropsFile = "./version.props";
-var doc = System.Xml.Linq.XDocument.Load(path);
+var currentDirectory = Directory.GetCurrentDirectory();
+Console.WriteLine($"currentDirectory: {currentDirectory}");
+
+var versionPropsFile = "./build/version.props";
+var doc = System.Xml.Linq.XDocument.Load(versionPropsFile);
 var propertyGroupNode = doc.Element("Project").Element("PropertyGroup");
 
 var version = $"{propertyGroupNode.Element("VersionMajor").Value}.{propertyGroupNode.Element("VersionMinor").Value}.{propertyGroupNode.Element("VersionPatch").Value}";
-Console.WriteLine($"Release version: {version}");
+Console.WriteLine($"Version: {version}");
 
 var envFile = Environment.GetEnvironmentVariable("GITHUB_ENV");
 Console.WriteLine($"EnvFilePath: {envFile}");
+
+if (string.IsNullOrEmpty(envFile)) return;
 
 File.WriteAllText(envFile, $"ReleaseVersion={version}");
 Console.WriteLine(File.ReadAllText(envFile));
