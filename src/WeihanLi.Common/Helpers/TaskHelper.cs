444060c21bfe9dc6539b186366af2ace0e1c6103
Weihan Li <weihanli@outlook.com>
2024-03-14 21:28:15 +0800

    feat: add TaskHelper.ToTask<T>

--- src/WeihanLi.Common/Helpers/TaskHelper.cs
+++ src/WeihanLi.Common/Helpers/TaskHelper.cs
@@ -15,4 +15,17 @@ public static class TaskHelper
         };
         return task;
     }
+
+    public static Task<T> ToTask<T>(object? obj, T defaultValue = default)
+    {
+        var task = obj switch
+        {
+            ValueTask<T> vt => vt.AsTask(),
+            Task<T> t => t,
+            ValueTask vt0 => vt0.AsTask().ContinueWith(_ => defaultValue),
+            Task t0 => t0.ContinueWith(_ => defaultValue),
+            _ => Task.FromResult(defaultValue)
+        };
+        return task;
+    }
 }
