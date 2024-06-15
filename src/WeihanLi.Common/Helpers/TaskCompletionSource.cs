10d4a1c6345449f6811e250544d76216fb5a67a1
Weihan Li <weihanli@outlook.com>
2024-06-15 12:52:39 +0800

    style: apply format

--- src/WeihanLi.Common/Helpers/TaskCompletionSource.cs
+++ src/WeihanLi.Common/Helpers/TaskCompletionSource.cs
@@ -1,9 +1,9 @@
-#if !NET5_0_OR_GREATER
+﻿#if !NET5_0_OR_GREATER
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
- 
+
 using System.Collections.Generic;
- 
+
 // ReSharper disable once CheckNamespace
 namespace System.Threading.Tasks;
 
