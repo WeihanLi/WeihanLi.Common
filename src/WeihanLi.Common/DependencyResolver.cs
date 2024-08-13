12798909fc35982420dde4ddccf983ac5777aa10
Weihan Li <weihanli@outlook.com>
2024-08-14 00:00:31 +0800

    refactor: update Lock usage

--- src/WeihanLi.Common/DependencyResolver.cs
+++ src/WeihanLi.Common/DependencyResolver.cs
@@ -9,7 +9,7 @@ namespace WeihanLi.Common;
 /// </summary>
 public static class DependencyResolver
 {
-    private static readonly object _lock = new();
+    private static readonly Lock _lock = new();
     public static IDependencyResolver Current { get; private set; } = new DefaultDependencyResolver();
 
     public static TService? ResolveService<TService>() => Current.ResolveService<TService>();
