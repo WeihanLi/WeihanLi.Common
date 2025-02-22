43f8ee30fa7d18fabbc9cffdd98afda16a049a07
Weihan Li <weihanli@outlook.com>
2025-02-22 21:06:31 +0800

    feat: add Assembly.GetDisplayVersion

--- src/WeihanLi.Common/Extensions/ReflectionExtension.cs
+++ src/WeihanLi.Common/Extensions/ReflectionExtension.cs
@@ -309,4 +309,37 @@ public static class ReflectionExtension
         Guard.NotNull(element);
         return Attribute.IsDefined(element, attributeType, inherit);
     }
+    
+    /// <summary>
+    /// Get Assembly Display Version
+    /// </summary>
+    /// <param name="assembly">assembly</param>
+    /// <returns>Version info</returns>
+    public static string? GetDisplayVersion(this Assembly assembly)
+    {
+        // The package version is stamped into the assembly's AssemblyInformationalVersionAttribute at build time, followed by a '+' and
+        // the commit hash, e.g.:
+        // [assembly: AssemblyInformationalVersion("8.0.0-preview.2.23604.7+e7762a46d31842884a0bc72c92e07ba700c99bf5")]
+
+        var version = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion;
+
+        if (version is not null)
+        {
+            var plusIndex = version.IndexOf('+');
+
+            if (plusIndex > 0)
+            {
+                return version[..plusIndex];
+            }
+
+            return version;
+        }
+
+        // Fallback to the file version, which is based on the CI build number, and then fallback to the assembly version, which is
+        // product stable version, e.g. 8.0.0.0
+        version = assembly.GetCustomAttribute<AssemblyFileVersionAttribute>()?.Version
+                  ?? assembly.GetCustomAttribute<AssemblyVersionAttribute>()?.Version;
+
+        return version;
+    }
 }
