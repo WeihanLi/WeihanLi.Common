8444d55c144964636087d007e41de2bd5811342e
Weihan Li <weihanli@outlook.com>
2025-11-12 03:04:44 +0800

    build: use dotnet run file to run build script

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-dotnet tool update -g dotnet-execute --prerelease
-export PATH="$PATH:$HOME/.dotnet/tools"
-
-echo "dotnet-exec ./build/build.cs --args $@"
-dotnet-exec ./build/build.cs --wide false --reference "project:./src/WeihanLi.Common/WeihanLi.Common.csproj" --using "WeihanLi.Common" --using "WeihanLi.Extensions" --using "WeihanLi.Common.Helpers" --args "$@"
