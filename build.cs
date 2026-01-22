b4d05b8e71cbe02b62bb2292d07c8881cef2671c
Weihan Li <weihanli@outlook.com>
2026-01-22 23:42:24 +0800

    build: test simplify

--- build.cs
+++ build.cs
@@ -1,4 +1,4 @@
-// Copyright (c) Weihan Li. All rights reserved.
+﻿// Copyright (c) Weihan Li. All rights reserved.
 // Licensed under the Apache license version 2.0 http://www.apache.org/licenses/LICENSE-2.0
 
 #:project ./src/WeihanLi.Common/WeihanLi.Common.csproj
